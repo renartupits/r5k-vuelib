a77ac6f6066ca311559e6115a6d91891b0ae4b74
Renar Tupits <renar.tupits@kwota.com>
2024-11-19 20:28:15 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main class="h-screen w-full bg-gray-800 text-white flex justify-center items-center">
+    <p>Start</p>
+  </main>
+</template>
---
+++ vite.config.ts
@@ -0,0 +1,31 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import vueDevTools from 'vite-plugin-vue-devtools'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueJsx(),
+    vueDevTools(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+  },
+  build: {
+    lib: {
+      entry: 'src/main.ts',
+      name: 'r5kvuelib',
+      fileName: 'r5k-vue-lib',
+      formats: ['es']
+    },
+    rollupOptions: {
+      external: ['vue']
+    }
+  }
+})
